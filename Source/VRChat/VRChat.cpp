ee9120d43426d4bce0441324e052792cf9bc5987
Cyrus <nickcyrus@163.com>
2021-01-02 21:54:27 +0800

    First commit

---
+++ Source/VRChat/VRChat.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "VRChat.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VRChat, "VRChat" );
---
+++ Source/VRChat/VRChatGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VRChatGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VRCHAT_API AVRChatGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
